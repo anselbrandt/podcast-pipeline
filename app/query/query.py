f6413f6f9eaa7aee54802f9c5e77bf2dcccb15aa
anselbrandt <ansel@anselbrandt.com>
2025-01-21 06:23:18 -0500

    Make number_chunks optional

--- app/query/query.py
+++ app/query/query.py
@@ -12,7 +12,7 @@ chromadb_dir = os.path.join(ROOT, "chromadb")
 model = SentenceTransformer("all-mpnet-base-v2")
 
 
-def query(query_string):
+def query(query_string, number_chunks="10"):
     chroma_client = chromadb.PersistentClient(path=chromadb_dir)
     sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
         model_name="all-mpnet-base-v2"
@@ -22,21 +22,29 @@ def query(query_string):
     )
     raw_results = collection.query(
         query_texts=[query_string],
-        n_results=30,
+        n_results=int(number_chunks),
         include=["documents", "metadatas"],
     )
     results = list(zip(raw_results["metadatas"][0], raw_results["documents"][0]))
     metas = [json.loads(metadatas["wavfiles"]) for metadatas, text in results]
     text = [f"{line["speaker"]}: {line["speech"]}" for chunk in metas for line in chunk]
-    print(text)
+    response = "\n".join(text)
+    print(response)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--query", type=str, help="Query string")
+    parser.add_argument(
+        "--chunks", type=str, required=False, help="Number of chunks to be returned"
+    )
     args = parser.parse_args()
     query_string = args.query
-    query(query_string)
+    if args.chunks is not None:
+        number_chunks = args.chunks
+        query(query_string, number_chunks)
+    else:
+        query(query_string)
 
 
 if __name__ == "__main__":
