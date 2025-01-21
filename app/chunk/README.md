4d923f16231179f9ee85aa25ea4369be80e55a92
anselbrandt <ansel@anselbrandt.com>
2025-01-21 02:54:59 -0500

    Add chunking dependencies

--- app/chunk/README.md
+++ app/chunk/README.md
@@ -1,5 +1,5 @@
 # Chunk
 
 ```
-python -m app.chunk --input files/output/segments.txt --show "Roderick on the Line" --episode 567 --title "A High Teen" --date "Monday, January 20, 2025"
+python -m app.chunk --input files/output/567_-_a_high_teen.srt --show "Roderick on the Line" --episode 567 --title "A High Teen" --date "Monday, January 20, 2025"
 ```
--- app/chunk/chunk.py
+++ app/chunk/chunk.py
@@ -1,13 +1,32 @@
+import os
 import argparse
+from dotenv import load_dotenv
+
+from semantic_router.encoders import OpenAIEncoder
+from semantic_router.splitters import RollingWindowSplitter
+
+from app.utils import srt_to_transcript
+
+load_dotenv()
+
+ROOT = os.getcwd()
+
+encoder = OpenAIEncoder(name="text-embedding-3-small")
 
 
 def chunk(file_path, show_name, episode_number, episode_title, episode_date):
-    print(file_path, show_name, episode_number, episode_title, episode_date)
+    chunkedDir = os.path.join(ROOT, "files", "chunked")
+    os.makedirs(chunkedDir, exist_ok=True)
+    transcript = srt_to_transcript(file_path)
+    content_with_speaker = [
+        f"{idx}|{speaker}: {speech} " for idx, start, end, speaker, speech in transcript
+    ]
+    print(content_with_speaker)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input", type=str, help="Path to labeled segments.txt file")
+    parser.add_argument("--input", type=str, help="Path to labeled .srt file")
     parser.add_argument("--show", type=str, help="Show name")
     parser.add_argument("--episode", type=str, help="Episode number")
     parser.add_argument("--title", type=str, help="Episode title")
