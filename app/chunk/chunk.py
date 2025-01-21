c48e546c8249547897116825b276b3eb25b083ee
anselbrandt <ansel@anselbrandt.com>
2025-01-21 03:16:13 -0500

    Add app.embed placeholder

--- app/chunk/chunk.py
+++ app/chunk/chunk.py
@@ -15,7 +15,7 @@ ROOT = os.getcwd()
 encoder = OpenAIEncoder(name="text-embedding-3-small")
 
 
-def chunk(file_path, show_name, episode_number, episode_title, episode_date):
+def chunk(file_path):
     chunkedDir = os.path.join(ROOT, "files", "chunked")
     os.makedirs(chunkedDir, exist_ok=True)
     transcript = srt_to_transcript(file_path)
@@ -45,18 +45,9 @@ def chunk(file_path, show_name, episode_number, episode_title, episode_date):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--input", type=str, help="Path to labeled .srt file")
-    parser.add_argument("--show", type=str, help="Show name")
-    parser.add_argument("--episode", type=str, help="Episode number")
-    parser.add_argument("--title", type=str, help="Episode title")
-    parser.add_argument("--date", type=str, help="Episode date")
     args = parser.parse_args()
     file_path = args.input
-    show_name = args.show
-    episode_number = args.episode
-    episode_title = args.title
-    episode_date = args.date
-
-    chunk(file_path, show_name, episode_number, episode_title, episode_date)
+    chunk(file_path)
 
 
 if __name__ == "__main__":
---
+++ app/embed/README.md
@@ -0,0 +1,5 @@
+# Embed
+
+```
+python -m app.embed --input files/output/567_-_a_high_teen.srt --show "Roderick on the Line" --episode 567 --title "A High Teen" --date "Monday, January 20, 2025"
+```
