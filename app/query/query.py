c8e90fbc36ac621b85e17b29e62641047420cf21
anselbrandt <ansel@anselbrandt.com>
2025-01-25 02:57:01 -0500

    Update app.query

--- app/query/query.py
+++ app/query/query.py
@@ -2,23 +2,23 @@ import os
 import argparse
 import json
 import chromadb
-from chromadb.utils import embedding_functions
-from sentence_transformers import SentenceTransformer
+from chromadb.utils.embedding_functions.sentence_transformer_embedding_function import (
+    SentenceTransformerEmbeddingFunction,
+)
+
 
 ROOT = os.getcwd()
 
 chromadb_dir = os.path.join(ROOT, "chromadb")
 
-model = SentenceTransformer("all-mpnet-base-v2")
-
 
 def query(query_string, number_chunks="10"):
     chroma_client = chromadb.PersistentClient(path=chromadb_dir)
-    sentence_transformer_ef = embedding_functions.SentenceTransformerEmbeddingFunction(
-        model_name="all-mpnet-base-v2"
+    embedding_function = SentenceTransformerEmbeddingFunction(
+        model_name="all-mpnet-base-v2", device="cuda"
     )
     collection = chroma_client.get_collection(
-        name="podcasts", embedding_function=sentence_transformer_ef
+        name="podcasts", embedding_function=embedding_function
     )
     raw_results = collection.query(
         query_texts=[query_string],
