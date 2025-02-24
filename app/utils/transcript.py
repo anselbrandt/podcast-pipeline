854f21ba0c80d01761cfc616595404fdb431dd04
anselbrandt <ansel@anselbrandt.com>
2025-02-24 14:45:10 -0800

    Update transcript utils

--- app/utils/transcript.py
+++ app/utils/transcript.py
@@ -32,7 +32,7 @@ def srt_to_transcript(filepath):
 
 
 def srt_to_lines(file):
-    with open(file, encoding="utf-8") as f:
+    with open(file, "r") as f:
         srt = f.read().strip().replace("\n\n", "\n").splitlines()
     entries = [srt[i : i + 3] for i in range(0, len(srt), 3)]
     lines = [line for idx, timestamp, line in entries]
@@ -40,7 +40,7 @@ def srt_to_lines(file):
 
 
 def srt_to_text(file):
-    with open(file, encoding="utf-8") as f:
+    with open(file, "r") as f:
         srt = f.read().strip().replace("\n\n", "\n").splitlines()
     entries = [srt[i : i + 3] for i in range(0, len(srt), 3)]
     lines = [line for idx, timestamp, line in entries]
